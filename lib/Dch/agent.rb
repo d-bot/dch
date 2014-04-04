7711997921629c2548db503b61597f0db7845f55
dch <dch@dch.io>
2014-04-04 09:47:29 +0000

    creating agent.rb

--- lib/Dch/agent.rb
+++ lib/Dch/agent.rb
@@ -1,5 +1,23 @@
 module Dch
-	class Agent
+	class Ping
+    include 'logger'
+    attr_reader :rtt
+    def initialize(domain)
+      @target = domain
+    end
+
+    def fping(option={})
+      res = `fping -q -c 5 #{@target.join(' ')} 2>&1 | awk '{print $1,$8}' | xargs`.split
+    end
+
+    def pingable?(domain)
+      ping_flag ||= nil
+
+      !!ping_flag
+    end
+	end
+
+	class Curl
 
 		attr_reader :http_code, :dns_lookup, :conn_time, :ttfb, :time_to_total
 
@@ -8,10 +26,9 @@ module Dch
 
 		end
 
-		def run
+		def rtt
 
 		end
-
 
 	end
 end
