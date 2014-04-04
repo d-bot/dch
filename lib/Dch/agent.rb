83bb3c6a4ac7f3b2e031ff69d14ce14bf07be853
dch <dch@dch.io>
2014-04-04 09:50:47 +0000

    ok

--- lib/Dch/agent.rb
+++ lib/Dch/agent.rb
@@ -19,10 +19,11 @@ module Dch
 
 	class Curl
 
-		attr_reader :http_code, :dns_lookup, :conn_time, :ttfb, :time_to_total
+		attr_reader :http_code, :dns_lookup, :tcp_handshake, :tls_handshake, :first_byte, :time_to_total
 
 		def initialize(url, r_byte, &block)
-			curl_cmd = %Q{ curl -s -D- -r 0-#{r_byte} -o /dev/null -H "Pragma: no-cache" -H "Cache-Control: no-cache" -H "User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2)" -w "%{http_code} DNSLookup: %{time_namelookup} Connect: %{time_connect} TTFB: %{time_starttransfer} Total time: %{time_total}" #{url}; echo }
+			fqdn = url.split('/')[2]
+			curl = %Q{ curl -I -s -o /dev/null -H "Host: #{fqdn}" -H "Accept-Encoding: gzip, deflate" -H "Pragma: no-cache" -H "Cache-Control: no-cache" -H "User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2)" -w "DNSLookup: %{time_namelookup} Handshake: %{time_connect} TLSHandshake: %{time_appconnect} TTFB: %{time_starttransfer} Total: %{time_total}" #{url} | cut -d ' ' -f2,4,6,8,10 }
 
 		end
 
